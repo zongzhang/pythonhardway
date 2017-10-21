117720eebb85521759910154fef7750222e73b32
zongzhang <jingchang_007@163.com>
2017-10-21 11:15:31 +0800

    添加练习

---
+++ ex45.py
@@ -0,0 +1,15 @@
+# -*-coding:utf-8-*-
+s = 'spam'
+
+print s.find('s')
+print s.find('p')
+print s.find('a')
+
+print s.find('am')
+
+print dir(s)
+
+lee = [1, 2, 3, 4]
+lee[0] = 9
+print lee[0]
+print lee
